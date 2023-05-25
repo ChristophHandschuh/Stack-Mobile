ca38a1e3ed084a87889f06e492e264266bdac285
ChrisiHand <69692985+ChrisiHand@users.noreply.github.com>
2023-05-25 20:17:31 +0200

    commit

--- lib/functions/learn_Algo.dart
+++ lib/functions/learn_Algo.dart
@@ -3,9 +3,7 @@ import 'package:hive/hive.dart';
 final storage = Hive.box('storage');
 
 Future<Map<String,dynamic>> learn_Algo(res, cards) async {
-  if(res == true){
-    //when res is true, it means the user got the question right
-    //the cards[0].time should be updated to 5 minutes in the future and sorted in the array with ascending time
+  if(res == true){ //card right
     cards[0]["time"] = DateTime.now().add(Duration(minutes: 5)).millisecondsSinceEpoch;
   }else if(res == false){
     //when res is false, it means the user got the question wrong
@@ -13,21 +11,23 @@ Future<Map<String,dynamic>> learn_Algo(res, cards) async {
     cards[0]["time"] = DateTime.now().add(Duration(minutes: 1)).millisecondsSinceEpoch;
   }
 
-  //sort cards with time descending
-  //print(cards);
-  var i = 0;
-  while(cards.length != 1 && i < cards.length-1 && cards[i]["time"] > cards[i+1]["time"]){
-      var temp = cards[i];
-      cards[i] = cards[i+1];
-      cards[i+1] = temp;
-      i++;
-  }
-  //print(cards);
+  sort_ascending(cards);
+  print(cards);
 
   //check if card.date is, or is more then 1 minute in the future
   if(DateTime.now().add(Duration(minutes: 1)).millisecondsSinceEpoch.toInt() > cards[0]["time"]){
     return await storage.get(cards[0]["card_id"]);
   }else{
     return {};
+  }
+}
+
+void sort_ascending(cards){
+  var i = 0;
+  while(cards.length != 1 && i < cards.length-1 && cards[i]["time"] > cards[i+1]["time"]){
+    var temp = cards[i];
+    cards[i] = cards[i+1];
+    cards[i+1] = temp;
+    i++;
   }
 }
